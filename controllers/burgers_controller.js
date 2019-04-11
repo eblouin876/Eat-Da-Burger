8d22259320a0ea4f5a84a5b075065c81ed991ab0
Emile Blouin <emile_blouin@alumni.brown.edu>
2019-04-10 21:07:54 -0700

    Building UI

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -1,14 +1,24 @@
 let express = require("express");
 let burger = require("../models/burger");
 let router = express.Router();
+let log = require("con-logger");
 
 router.get("/", async function(req, res) {
   let burgers = await burger.getBurgers();
-  console.log(burgers);
+
+  for (let key in burgers) {
+    if (burgers[key].devoured === 0) {
+      burgers[key].devoured = false;
+    } else {
+      burgers[key].devoured = true;
+    }
+  }
   res.render("index", { burgers });
 });
 
-router.post("/api/burgers", function(req, res) {});
+router.post("/api/burgers", function(req, res) {
+  log(req.body);
+});
 
 router.put("/api/burgers/:name", function(req, res) {});
 
--- db/init.js
+++ db/init.js
@@ -1,37 +1,31 @@
 let Database = require("../config/orm");
 
+let burgers_db = "";
 async function init() {
-  let burgers_db = new Database("burgers_db");
-  console.log("Here");
+  burgers_db = new Database("burgers_db");
+  await burgers_db.connect();
+
+  burgers_db
+    .query(
+      "CREATE TABLE IF NOT EXISTS burgers_db.burgers (id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, burger VARCHAR(50), devoured BOOLEAN DEFAULT false )"
+    )
+    .catch(err => console.log(err));
+
   await burgers_db
-    .makeTable("burgers", {
-      id: "INT NOT NULL AUTO_INCREMENT PRIMARY KEY",
-      burger: "VARCHAR(50)",
-      devoured: "VARCHAR(5)"
-    })
+    .create("burgers", { burger: "Ghost Chili Burger" })
     .catch(err => console.log(err));
-  console.log("Here");
 
-  burgers_db
-    .create("burgers", { burger: "Ghost Chili Burger", devoured: false })
-    .catch(err => console.log(err));
-  console.log("Here");
-
-  burgers_db
+  await burgers_db
     .create("burgers", {
-      burger: "Sauteed Onion and Mushroom Burger",
-      devoured: false
+      burger: "Sauteed Onion and Mushroom Burger"
     })
     .catch(err => console.log(err));
-  console.log("Here");
 
-  burgers_db
+  await burgers_db
     .create("burgers", {
-      burger: "Black Bean and Quinoa Burger",
-      devoured: false
+      burger: "Black Bean and Quinoa Burger"
     })
     .catch(err => console.log(err));
-  console.log("Here");
 
   burgers_db.read("burgers", "*").then(res => console.log(res));
 }
